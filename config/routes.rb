dc9a93fee338d0009faf459cd9cd9a9000cf1e56
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2012-11-24 01:12:04 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 MovieMarathonHelper::Application.routes.draw do
+  get "users/new"
+
   root to: "static_pages#home"
 
   match '/help', to: 'static_pages#help'
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
