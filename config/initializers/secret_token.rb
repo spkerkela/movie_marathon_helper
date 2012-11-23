2983397e73cab76932bd54a015fdf21fd2fc1c57
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2012-11-23 20:06:27 +0200

    Initial moviemaker commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieMarathonHelper::Application.config.secret_token = '6dcbe77c6ddd8dc8307d20cc6a619648797e0e7731b09c11a831ee8c933f1e2d6c8d50ebd579b3ff29857c38fc7c892984c90fa6bd41c243e683b33834afbca6'
