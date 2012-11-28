189b0b047db0d91ad27166f43bd33b3db0f0de2f
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2012-11-28 21:56:13 +0200

    Finish user edit, update, index and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :show]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :show, :destroy]
   before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
 
   def show
 		@user = User.find(params[:id])
@@ -11,6 +12,12 @@ class UsersController < ApplicationController
 	end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def update
@@ -50,5 +57,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_path unless current_user? @user
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 end
