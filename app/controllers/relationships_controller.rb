5ae55873c9f60527ebb0c636b7414e38b4a62f8d
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2012-12-14 23:26:40 +0200

    Disabled ajax cos bugs

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,18 @@ class RelationshipsController < ApplicationController
 	before_filter :signed_in_user
 
 
-	respond_to :html, :js
+#	respond_to :html, :js
 	def create
 		@user = User.find(params[:relationship][:followed_id])
 		current_user.follow!(@user)
-		respond_with @user
+		# respond_with @user
+		redirect_to @user
 	end
 
 	def destroy
 		@user = Relationship.find(params[:id]).followed
 		current_user.unfollow!(@user)
-		respond_with @user
+		# respond_with @user
+		redirect_to @user
 	end
 end
