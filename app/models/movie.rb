7010b2d16ccd8dbc50099cd661d9e63ed1007b1d
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2012-12-17 17:20:49 +0200

    ordered movies by title name

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -25,4 +25,6 @@ class Movie < ActiveRecord::Base
   attr_accessible :actors, :director, :genre, :imdbid, :imdbrating, :imdbvotes, :plot, :poster, :rated, :released, :runtime, :title, :writer, :year
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :year, presence: true
+
+  default_scope order: 'movies.title ASC'
 end
